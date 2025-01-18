e4745e0d2bc92dab9941aa5479735c53a06841ff
MatiasMarchantQ <matias.marchantq@gmail.com>
2025-01-18 05:48:32 -0300

    02

--- src/Pages/Home/FichaClinica.js
+++ src/Pages/Home/FichaClinica.js
@@ -5,7 +5,7 @@ import { useAuth } from '../../contexts/AuthContext';
 import _ from 'lodash';
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './styles/FichaClinica.css';
+import '../styles/FichaClinica.css';
 
 import SeguimientoInfantil from './SeguimientoInfantil';
 import SeguimientoAdulto from './SeguimientoAdulto';
